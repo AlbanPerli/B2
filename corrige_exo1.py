8051f426b85d977acd2efb37b5827e176a503832
AlbanP <alban.perli@gmail.com>
2025-12-09 16:15:03 +0100

    Add files via upload

--- corrige_exo1.py
+++ corrige_exo1.py
@@ -175,8 +175,6 @@ class FinishedState(PlayableState):
         self._transition(StationaryState)
 
 
-
-
 class MovementState(ABC):
 
     def __init__(self, hero):
@@ -236,36 +234,123 @@ class MovingState(MovementState):
 
 
 
+class HeroState(ABC):
+
+    def __init__(self, hero):
+        self.hero = hero
+
+    def on_enter(self, from_state: "HeroState | None"):
+        print(f"[ENTER] {self.name}"
+              + (f" (from {from_state.name})" if from_state else ""))
+
+    def on_exit(self, to_state: "HeroState"):
+        print(f"[EXIT]  {self.name} -> {to_state.name}")
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+     # utilitaire
+    def _transition(self, new_state_cls: type["HeroState"], ):
+        self.hero.set_state(new_state_cls)
+
+    def to_movement(self):
+        pass
+
+    def to_stationary(self):
+        pass
+
+    def to_finished(self):
+        pass
+
+class MovingHeroState(HeroState):
+
+    @property
+    def name(self) -> str:
+        return "MOVING"
+    
+
+    def on_enter(self, from_state: "HeroState | None"):
+        print(f"[ENTER] {self.name}"
+              + (f" (from {from_state.name})" if from_state else ""))
+
+    def on_exit(self, to_state: "HeroState"):
+        print(f"[EXIT]  {self.name} -> {to_state.name}")
+    
+
+
+    def to_movement(self):
+        # déjà en MOVING
+        print("Déjà en MOVING, aucune transition.")
+
+    def to_stationary(self):
+        self._transition(StationaryHeroState)
+
+    def to_finished(self):
+        self._transition(FinishedHeroState)
+
+
+class StationaryHeroState(HeroState):
+
+    @property
+    def name(self) -> str:
+        return "STATIONARY"
+    
+    def to_stationary(self):
+        # déjà en STATIONARY
+        print("Déjà en STATIONARY, aucune transition.")
+
+    def to_movement(self):
+        self._transition(MovingHeroState)
+
+    def to_finished(self):
+        self._transition(FinishedHeroState)
+
+class FinishedHeroState(HeroState):
+
+    @property
+    def name(self) -> str:
+        return "FINISHED"
+    
+    def to_finished(self):
+        # déjà en FINISHED
+        print("Déjà en FINISHED, aucune transition.")
+
+    def to_movement(self):
+         print("Déjà en FINISHED, aucune transition.")
+
+    def to_stationary(self):
+         print("Déjà en FINISHED, aucune transition.")
+
+
+
 class Hero:
     def __init__(self):
-        self.moving_state: MovementState = StationaryState(self)
-        self.playing_state: PlayableState = PlayingState(self)
-        self.moving_state.on_enter(None)
-        self.playing_state.on_enter(None)
+        self.hero_state:HeroState = StationaryHeroState(self)
+        self.hero_state.on_enter(None)
 
     # Modifier set_state ou créer une nouvelle méthode, pour gérer les 3 types d'état
-    def set_state(self, new_state_cls: type[MovementState], action=None):
-        old_state = self.moving_state
+    def set_state(self, new_state_cls, action=None):
+        old_state = self.hero_state
         new_state = new_state_cls(self)
-
         # Hooks de sortie / entrée
         old_state.on_exit(new_state)
-        
         new_state.on_enter(old_state)
 
-        self.moving_state = new_state
+        self.hero_state = new_state
+
+
 
 hero = Hero()
-hero.moving_state.to_movement()
-hero.playing_state.to_finished()
-print("----")
-print(f"État actuel du héros : {hero.moving_state.name}")
-print("----")
-hero.moving_state.to_movement()
-print("----")
-print(f"État actuel du héros : {hero.moving_state.name}")
-print("----")
-hero.moving_state.to_stationary()
-print("----")
-print(f"État actuel du héros : {hero.moving_state.name}")
-print("----")
+print("Hero initial state:", hero.hero_state.name)
+hero.hero_state.to_movement()
+print("Hero current state:", hero.hero_state.name)
+hero.hero_state.to_movement()
+print("Hero current state:", hero.hero_state.name)
+hero.hero_state.to_finished()
+print("Hero current state:", hero.hero_state.name)
+hero.hero_state.to_movement()
+print("Hero current state:", hero.hero_state.name)
+hero.hero_state.to_stationary()
+print("Hero current state:", hero.hero_state.name)
